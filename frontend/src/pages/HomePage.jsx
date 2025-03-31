998852fc3711d1621c4301417a028206d7f65755
yairzzzz <yairshabtai555@gmail.com>
2025-03-31 18:45:05 +0300

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
   return (
    <div className="h-[100dvh] bg-base-200">
       <div className="flex items-center justify-center pt-20 px-4">
-        <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl h-[calc(100vh-8rem)]">
+ <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl h-[calc(100dvh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
             <Sidebar />
             {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
